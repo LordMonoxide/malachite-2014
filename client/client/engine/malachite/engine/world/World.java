419fa3a27b43ca738a1979623a2b211acdfc9ff8
Corey Frenette <corey@narwhunderful.com>
2014-06-14 13:00:33 -0300

    Allow iteration of world

--- client/client/engine/malachite/engine/world/World.java
+++ client/client/engine/malachite/engine/world/World.java
@@ -1,14 +1,23 @@
 package malachite.engine.world;
 
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
 
 import malachite.engine.world.entity.Entity;
 
-public class World {
+public class World implements Iterable<Entity> {
   private List<Entity> _entity = new ArrayList<>();
   
   public void add(Entity e) {
     _entity.add(e);
+  }
+  
+  @Override public Iterator<Entity> iterator() {
+    return _entity.iterator();
+  }
+  
+  public static interface Listener {
+    
   }
 }
