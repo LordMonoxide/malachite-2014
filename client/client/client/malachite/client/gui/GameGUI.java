6edae5ac521e432817e034f6f10e0d2937ab448c
Corey Frenette <corey@narwhunderful.com>
2014-06-18 11:34:50 -0300

    Remove test code

--- client/client/client/malachite/client/gui/GameGUI.java
+++ client/client/client/malachite/client/gui/GameGUI.java
@@ -6,15 +6,10 @@ import malachite.api.Settings;
 import malachite.client.game.Game;
 import malachite.client.gfx.RegionRenderer;
 import malachite.engine.world.Region;
-import malachite.gfx.Context;
-import malachite.gfx.Drawable;
-import malachite.gfx.ShaderBuilder;
 import malachite.gfx.fonts.Font;
 import malachite.gfx.fonts.FontBuilder;
 import malachite.gfx.fonts.TextStream;
 import malachite.gfx.gui.GUI;
-import malachite.gfx.textures.Canvas;
-import malachite.gfx.textures.TextureBuilder;
 
 public class GameGUI extends GUI {
   private static final Font _font = FontBuilder.getInstance().getDefault();
@@ -23,20 +18,13 @@ public class GameGUI extends GUI {
   
   private HashMap<String, RegionRenderer> _region = new HashMap<>();
   
-  private Drawable test;
-  private Canvas c;
-  
   public GameGUI(Game.GameInterface gi) {
     gameInterface = gi;
     ready();
   }
   
   @Override protected void load() {
-    test = Context.newDrawable();
-    test.setTexture(TextureBuilder.getInstance().getTexture("tiles/0.png"));
-    test.setXYWH(100, 100, 256, 256);
-    //test.setProgram(ShaderBuilder.getInstance().getProgram("default.vsh", "default.fsh"));
-    test.createQuad();
+    
   }
   
   @Override public void destroy() {
@@ -62,8 +50,6 @@ public class GameGUI extends GUI {
     for(int z = 0; z < Settings.Map.Depth; z++) {
       getRegion(gameInterface.me().getRegion()).draw(z);
     }
-    
-    test.draw();
     
     _font.draw(4, 4, new TextStream(String.valueOf(Math.round(_context.getFPS())) + " FPS")); //$NON-NLS-1$
   }
