6048b50914b6b6906492746312628d12a2c09d3d
Corey Frenette <corey@narwhunderful.com>
2014-06-19 15:06:25 -0300

    Auth API

--- http/app/routes.php
+++ http/app/routes.php
@@ -4,6 +4,13 @@ Route::group(['prefix' => 'api'], function() {
   Route::group(['prefix' => 'lang'], function() {
     Route::get('/app', ['as' => 'api.lang.app', 'uses' => 'api\lang\LangController@app']);
   });
+  
+  Route::group(['prefix' => 'auth'], function() {
+    Route::get ('/check',    ['as' => 'api.auth.check',    'uses' => 'api\auth\AuthController@check']);
+    Route::put ('/register', ['as' => 'api.auth.register', 'uses' => 'api\auth\AuthController@register']);
+    Route::post('/login',    ['as' => 'api.auth.login',    'uses' => 'api\auth\AuthController@login']);
+    Route::post('/logout',   ['as' => 'api.auth.logout',   'uses' => 'api\auth\AuthController@logout']);
+  });
 });
 
 Route::get('/', ['as' => 'home', 'uses' => 'HomeController@home']);
