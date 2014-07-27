c1df0ebac1410f72900bb9dea1b500a4fa72e6c8
Corey Frenette <corey@narwhunderful.com>
2014-07-27 14:42:28 -0300

    Finish first iteration of GUIParser

--- client/gfx/src/gfx/gui/GUIParser.java
+++ client/gfx/src/gfx/gui/GUIParser.java
@@ -16,8 +16,11 @@ import java.lang.reflect.Method;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
-import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
@@ -51,6 +54,8 @@ public class GUIParser {
   }
   
   private GUI _gui;
+  private List<AssignLater> _assignLater = new ArrayList<>();
+  private Map<String, Control<?>> _controls = new HashMap<>();
   
   public GUI loadFromFile(Path f) throws IOException, JSONException, SecurityException, IllegalArgumentException {
     byte[] raw = Files.readAllBytes(f);
@@ -72,11 +77,17 @@ public class GUIParser {
         for(String key : json.keySet()) {
           if(key.equals("controls")) {
             try {
-              addControls(_gui.controls(), json.getJSONArray("controls"));
+              addControls(_gui.controls(), json.getJSONObject("controls"));
             } catch(InstantiationException | IllegalAccessException | ClassNotFoundException | NoSuchMethodException | SecurityException | IllegalArgumentException | InvocationTargetException | JSONException e) {
               e.printStackTrace();
             }
           }
+        }
+        
+        try {
+          processLateAssignment();
+        } catch(IllegalAccessException | IllegalArgumentException | InvocationTargetException | InstantiationException | ClassNotFoundException | NoSuchMethodException | SecurityException | JSONException e) {
+          e.printStackTrace();
         }
       }
       
@@ -93,23 +104,25 @@ public class GUIParser {
     return _gui;
   }
   
-  private void addControls(ControlList controls, JSONArray array) throws InstantiationException, IllegalAccessException, ClassNotFoundException, NoSuchMethodException, SecurityException, IllegalArgumentException, InvocationTargetException {
-    for(int i = 0; i < array.length(); i++) {
-      JSONObject control = array.getJSONObject(i);
+  private void addControls(ControlList controls, JSONObject json) throws InstantiationException, IllegalAccessException, ClassNotFoundException, NoSuchMethodException, SecurityException, IllegalArgumentException, InvocationTargetException {
+    for(String name : json.keySet()) {
+      JSONObject attribs = json.getJSONObject(name);
       
       String controlType;
+      
       try {
-        controlType = control.getString("type");
+        controlType = attribs.getString("type");
       } catch(JSONException e) {
-        controlType = control.getString("Type");
+        controlType = attribs.getString("Type");
       }
       
       controlType = snakeToProper(controlType);
       
       Control<?> c = Class.forName("gfx.gui.control." + controlType).asSubclass(Control.class).newInstance();
       controls.add(c);
+      _controls.put(name, c);
       
-      parseAttribs(c, control);
+      parseAttribs(c, attribs);
     }
   }
   
@@ -123,7 +136,7 @@ public class GUIParser {
             return;
           }
           
-          addControls(((Control<?>)c).controls(), attribs.getJSONArray(attrib));
+          addControls(((Control<?>)c).controls(), attribs.getJSONObject(attrib));
           break;
           
         default:
@@ -133,40 +146,80 @@ public class GUIParser {
     }
   }
   
-  private void parseAttrib(Object c, String attrib, Object value) throws IllegalAccessException, IllegalArgumentException, InvocationTargetException, InstantiationException, ClassNotFoundException, NoSuchMethodException, SecurityException, JSONException {
+  private void parseAttrib(Object obj, String attrib, Object value) throws IllegalAccessException, IllegalArgumentException, InvocationTargetException, InstantiationException, ClassNotFoundException, NoSuchMethodException, SecurityException, JSONException {
+    // Deduce type
     Class<?> type = value.getClass();
     if(type == Integer.class) { type = int.class; }
     if(type == Boolean.class) { type = boolean.class; }
-    if(type == String .class) {
-      String s = (String)value;
-      if(s.startsWith("@")) {
-        
-      }
-      
-      if(s.startsWith("#")) {
-        String path = s.substring(1).replace('.', '/') + ".png";
-        value = TextureBuilder.getInstance().getTexture(path);
-        type = Texture.class;
-      }
-    }
     
-    String methodName = snakeToCamel(attrib);
-    Member member = findMethodOrFieldByName(c.getClass(), methodName, type);
+    Member member = findMethodOrFieldByName(obj.getClass(), snakeToCamel(attrib), type);
     
     if(member != null) {
-      if(member instanceof Field) {
-        if(!(value instanceof JSONObject)) {
-          System.err.println("Value of a field must be a JSONObject");
+      if(type == String .class) {
+        String s = (String)value;
+        if(s.startsWith("@")) {
+          _assignLater.add(new AssignLater(obj, member, (String)value));
           return;
         }
         
-        parseAttribs(((Field)member).get(c), (JSONObject)value);
-      } else if(member instanceof Method) {
-        Method method = (Method)member;
-        method.invoke(c, value);
+        if(s.startsWith("#")) {
+          String path = s.substring(1).replace('.', '/') + ".png";
+          value = TextureBuilder.getInstance().getTexture(path);
+          type = Texture.class;
+        }
       }
+      
+      assignValue(obj, member, value);
     } else {
-      System.err.println("Couldn't find field/method " + methodName + "!");
+      System.err.println("Couldn't find field/method " + attrib + "!");
+    }
+  }
+  
+  private void assignValue(Object obj, Member member, Object value) throws InstantiationException, IllegalAccessException, ClassNotFoundException, NoSuchMethodException, SecurityException, IllegalArgumentException, InvocationTargetException, JSONException {
+    if(member instanceof Field) {
+      if(!(value instanceof JSONObject)) {
+        System.err.println("Value of a field must be a JSONObject");
+        return;
+      }
+      
+      parseAttribs(((Field)member).get(obj), (JSONObject)value);
+    } else if(member instanceof Method) {
+      Method method = (Method)member;
+      method.invoke(obj, value);
+    }
+  }
+  
+  private void processLateAssignment() throws IllegalAccessException, IllegalArgumentException, InvocationTargetException, InstantiationException, ClassNotFoundException, NoSuchMethodException, SecurityException, JSONException {
+    for(AssignLater late : _assignLater) {
+      String[] parts = late.value.substring(1).split("\\.");
+      
+      Object value = null;
+      for(String part : parts) {
+        if(value == null) {
+          value = _controls.get(part);
+        } else {
+          Member member = findMethodOrFieldByName(value.getClass(), snakeToCamel(part));
+          if(member instanceof Method) {
+            value = ((Method)member).invoke(value);
+          } else if(member instanceof Field) {
+            value = ((Field)member).get(value);
+          }
+        }
+      }
+      
+      assignValue(late.obj, late.member, value);
+    }
+  }
+  
+  private class AssignLater {
+    public Object obj;
+    public Member member;
+    public String value;
+    
+    public AssignLater(Object obj, Member member, String value) {
+      this.obj = obj;
+      this.member = member;
+      this.value = value;
     }
   }
 }
