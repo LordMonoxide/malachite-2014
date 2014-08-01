b92da41bbf899870a73d93c756ddca6d98854302
Corey Frenette <corey@narwhunderful.com>
2014-08-01 01:10:21 -0300

    Test stuff for bounds

--- client/sexy_adventures/src/game/MainMenuEvents.java
+++ client/sexy_adventures/src/game/MainMenuEvents.java
@@ -1,8 +1,9 @@
 package game;
 
 public class MainMenuEvents {
-  public void login(String email, String password) {
+  public void login(String email, String password, Object test) {
     System.out.println(email);
     System.out.println(password);
+    System.out.println(test);
   }
 }
