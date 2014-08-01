4e074821a1e1de096bef971af34b134f21a841b9
Corey Frenette <corey@narwhunderful.com>
2014-08-01 01:09:47 -0300

    Use calculated x/y values

--- client/gfx/src/malachite/gfx/util/Points.java
+++ client/gfx/src/malachite/gfx/util/Points.java
@@ -4,14 +4,14 @@ public final class Points {
   private Points() { }
   
   public static Point add(Point p1, Point p2) {
-    return new Point(p1._x + p2._x, p1._y + p2._y);
+    return new Point(p1.getX() + p2.getX(), p1.getY() + p2.getY());
   }
   
   public static Point subtract(Point p1, Point p2) {
-    return new Point(p1._x - p2._x, p1._y - p2._y);
+    return new Point(p1.getX() - p2.getX(), p1.getY() - p2.getY());
   }
   
   public static Point mean(Point p1, Point p2) {
-    return new Point((p2._x - p1._x) / 2, (p2._y - p1._y) / 2);
+    return new Point((p2.getX() - p1.getX()) / 2, (p2.getY() - p1.getY()) / 2);
   }
 }
