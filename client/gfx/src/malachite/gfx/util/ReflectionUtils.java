1fa0f32bcef9512e8810e9cebdaad48fbe61d7e4
Corey Frenette <corey@narwhunderful.com>
2014-08-02 00:15:21 -0300

    Allow multiple arg hints and try to find best-match method

--- client/gfx/src/malachite/gfx/util/ReflectionUtils.java
+++ client/gfx/src/malachite/gfx/util/ReflectionUtils.java
@@ -3,23 +3,17 @@ package malachite.gfx.util;
 import java.lang.reflect.Field;
 import java.lang.reflect.Member;
 import java.lang.reflect.Method;
+import java.util.ArrayList;
+import java.util.List;
 
 import static malachite.gfx.util.StringUtils.*;
 
 public class ReflectionUtils {
-  public static Member findMethodOrFieldByName(Class<?> c, String name) {
-    return findMethodOrFieldByName(c, name, null);
+  public static Member findMethodOrFieldByName(Class<?> c, String name, Class<?>... argHint) {
+    return findMethodOrFieldByName(c, name, true, true, argHint);
   }
   
-  public static Member findMethodOrFieldByName(Class<?> c, String name, boolean withGets, boolean withSets) {
-    return findMethodOrFieldByName(c, name, null, withGets, withSets);
-  }
-  
-  public static Member findMethodOrFieldByName(Class<?> c, String name, Class<?> argHint) {
-    return findMethodOrFieldByName(c, name, argHint, true, true);
-  }
-  
-  public static Member findMethodOrFieldByName(Class<?> c, String name, Class<?> argHint, boolean withGets, boolean withSets) {
+  public static Member findMethodOrFieldByName(Class<?> c, String name, boolean withGets, boolean withSets, Class<?>... argHint) {
     Member member = findFieldByName(c, name);
     if(member != null) { return member; }
     
@@ -50,7 +44,7 @@ public class ReflectionUtils {
     return null;
   }
   
-  public static Method findMethodByName(Class<?> c, String name, Class<?> argHint) {
+  public static Method findMethodByName(Class<?> c, String name, Class<?>... argHint) {
     if(argHint != null) {
       try {
         return c.getMethod(name, argHint);
@@ -61,19 +55,69 @@ public class ReflectionUtils {
       }
     }
     
-    Method candidate = null;
+    List<MethodAccuracy> methods = new ArrayList<>();
+    
     for(Method m : c.getMethods()) {
-      if(m.getName().equalsIgnoreCase(name)) {
-        if(argHint == null || (m.getParameterCount() != 0 && argHint.isAssignableFrom(m.getParameterTypes()[0]))) {
-          return m;
-        } else {
-          if(candidate == null) {
-            candidate = m;
+      boolean methodExists = false;
+      
+      int total = 0;
+      int accuracy = 0;
+      
+      total += 2;
+      
+      if(m.getName().equals(name)) {
+        methodExists = true;
+        accuracy += 2;
+      } else if(m.getName().equalsIgnoreCase(name)) {
+        methodExists = true;
+        accuracy += 1;
+      }
+      
+      if(methodExists) {
+        if(argHint.length == m.getParameterCount()) {
+          total += 10;
+          accuracy += 10;
+        }
+        
+        for(int i = 0; i < java.lang.Math.min(argHint.length, m.getParameterCount()); i++) {
+          total += 2;
+          
+          if(argHint[i] == m.getParameterTypes()[i]) {
+            accuracy += 2;
+          } else if(argHint[i].isAssignableFrom(m.getParameterTypes()[i])) {
+            accuracy += 1;
           }
+        }
+        
+        methods.add(new MethodAccuracy(m, accuracy / total));
+      }
+    }
+    
+    MethodAccuracy mostAccurate = null;
+    for(MethodAccuracy m : methods) {
+      if(mostAccurate == null) {
+        mostAccurate = m;
+      } else {
+        if(m.accuracy > mostAccurate.accuracy) {
+          mostAccurate = m;
         }
       }
     }
     
-    return candidate;
+    if(mostAccurate != null) {
+      return mostAccurate.method;
+    }
+    
+    return null;
+  }
+  
+  private static class MethodAccuracy {
+    public final Method method;
+    public final float  accuracy;
+    
+    public MethodAccuracy(Method method, float accuracy) {
+      this.method   = method;
+      this.accuracy = accuracy;
+    }
   }
 }
