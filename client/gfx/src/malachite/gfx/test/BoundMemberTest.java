c640ec5f6b825bba69a90f06b200cde9089aeba9
Corey Frenette <corey@narwhunderful.com>
2014-08-11 15:38:34 -0300

    Fix all kinds of strict warnings

--- client/gfx/src/malachite/gfx/test/BoundMemberTest.java
+++ client/gfx/src/malachite/gfx/test/BoundMemberTest.java
@@ -25,11 +25,11 @@ public class BoundMemberTest {
   @Test
   public void basicConstructor() {
     SimpleObject object = new SimpleObject();
-    Method methodGet = ReflectionUtils.findMethodByName(object.getClass(), "getInt");
-    Method methodSet = ReflectionUtils.findMethodByName(object.getClass(), "setInt");
+    Method methodGet = ReflectionUtils.findMethodByName(object.getClass(), "getInt"); //$NON-NLS-1$
+    Method methodSet = ReflectionUtils.findMethodByName(object.getClass(), "setInt"); //$NON-NLS-1$
     
-    assertEquals("getInt was actually " + methodGet, "getInt", methodGet.getName());
-    assertEquals("setInt was actually " + methodSet, "setInt", methodSet.getName());
+    assertEquals("getInt was actually " + methodGet, "getInt", methodGet.getName()); //$NON-NLS-1$ //$NON-NLS-2$
+    assertEquals("setInt was actually " + methodSet, "setInt", methodSet.getName()); //$NON-NLS-1$ //$NON-NLS-2$
     
     methodGet.setAccessible(true);
     methodSet.setAccessible(true);
@@ -37,27 +37,27 @@ public class BoundMemberTest {
     BoundMember get = new BoundMember(object, methodGet);
     BoundMember set = new BoundMember(object, methodSet);
     
-    assertEquals("Object gets are inequal", object, get.object);
-    assertEquals("Object sets are inequal", object, set.object);
-    assertEquals("Gets are inequal", methodGet, get.member);
-    assertEquals("Sets are inequal", methodSet, set.member);
+    assertEquals("Object gets are inequal", object, get.object); //$NON-NLS-1$
+    assertEquals("Object sets are inequal", object, set.object); //$NON-NLS-1$
+    assertEquals("Gets are inequal", methodGet, get.member); //$NON-NLS-1$
+    assertEquals("Sets are inequal", methodSet, set.member); //$NON-NLS-1$
     
     try {
-      set.setValue(1);
+      set.setValue(Integer.valueOf(1));
     } catch(IllegalAccessException | InvocationTargetException e) {
       e.printStackTrace();
-      fail("Set failed");
+      fail("Set failed"); //$NON-NLS-1$
     } catch(IllegalArgumentException e) {
-      fail("Invalid arguments for " + set);
+      fail("Invalid arguments for " + set); //$NON-NLS-1$
     }
     
     try {
-      assertEquals("Value should have been 1", 1, get.getValue());
+      assertEquals("Value should have been 1", Integer.valueOf(1), get.getValue()); //$NON-NLS-1$
     } catch(IllegalAccessException | InvocationTargetException e) {
       e.printStackTrace();
-      fail("Get failed");
+      fail("Get failed"); //$NON-NLS-1$
     } catch(IllegalArgumentException e) {
-      fail("Invalid arguments for " + get);
+      fail("Invalid arguments for " + get); //$NON-NLS-1$
     }
   }
   
@@ -68,53 +68,53 @@ public class BoundMemberTest {
     BoundMember set = null;
     
     try {
-      get = new BoundMember(object, "int", BoundMember.Type.ACCESSOR);
+      get = new BoundMember(object, "int", BoundMember.Type.ACCESSOR); //$NON-NLS-1$
     } catch(IllegalAccessException e) {
       e.printStackTrace();
-      fail("Illegal access in get " + get);
+      fail("Illegal access in get " + get); //$NON-NLS-1$
     } catch(InvocationTargetException e) {
       e.printStackTrace();
-      fail("An error was thrown in a path accessor in get " + get);
+      fail("An error was thrown in a path accessor in get " + get); //$NON-NLS-1$
     } catch(IllegalArgumentException e) {
       e.printStackTrace();
-      fail("An illegal argument was passed to an accessor of get " + get);
+      fail("An illegal argument was passed to an accessor of get " + get); //$NON-NLS-1$
     } catch(NoSuchMethodException e) {
       e.printStackTrace();
-      fail("There is no such method in get " + get);
+      fail("There is no such method in get " + get); //$NON-NLS-1$
     }
     
     assertNotNull(get);
     
     try {
-      set = new BoundMember(object, "int", BoundMember.Type.MUTATOR);
+      set = new BoundMember(object, "int", BoundMember.Type.MUTATOR); //$NON-NLS-1$
     } catch(IllegalAccessException e) {
       e.printStackTrace();
-      fail("Illegal access in set " + set);
+      fail("Illegal access in set " + set); //$NON-NLS-1$
     } catch(InvocationTargetException e) {
       e.printStackTrace();
-      fail("An exception was thrown in a path accessor in set " + set);
+      fail("An exception was thrown in a path accessor in set " + set); //$NON-NLS-1$
     } catch(IllegalArgumentException e) {
       e.printStackTrace();
-      fail("An illegal argument was passed to an accessor of set " + set);
+      fail("An illegal argument was passed to an accessor of set " + set); //$NON-NLS-1$
     } catch(NoSuchMethodException e) {
       e.printStackTrace();
-      fail("There is no such method in set " + set);
+      fail("There is no such method in set " + set); //$NON-NLS-1$
     }
     
     assertNotNull(set);
     
     try {
-      set.setValue(1);
-      assertEquals("Value should have been 1", 1, get.getValue());
+      set.setValue(Integer.valueOf(1));
+      assertEquals("Value should have been 1", Integer.valueOf(1), get.getValue()); //$NON-NLS-1$
     } catch(IllegalAccessException e) {
       e.printStackTrace();
-      fail("Illegal access while setting value");
+      fail("Illegal access while setting value"); //$NON-NLS-1$
     } catch(IllegalArgumentException e) {
       e.printStackTrace();
-      fail("Illegal argument while setting value");
+      fail("Illegal argument while setting value"); //$NON-NLS-1$
     } catch(InvocationTargetException e) {
       e.printStackTrace();
-      fail("An exception was thrown in value setter");
+      fail("An exception was thrown in value setter"); //$NON-NLS-1$
     }
   }
   
@@ -124,34 +124,34 @@ public class BoundMemberTest {
     BoundMember list = null;
     
     try {
-      list = new BoundMember(object, "list.size", BoundMember.Type.NEITHER);
+      list = new BoundMember(object, "list.size", BoundMember.Type.NEITHER); //$NON-NLS-1$
     } catch(IllegalAccessException e) {
       e.printStackTrace();
-      fail("Illegal access while finding member");
+      fail("Illegal access while finding member"); //$NON-NLS-1$
     } catch(InvocationTargetException e) {
       e.printStackTrace();
-      fail("An exception was thrown in a path accessor while finding member");
+      fail("An exception was thrown in a path accessor while finding member"); //$NON-NLS-1$
     } catch(IllegalArgumentException e) {
       e.printStackTrace();
-      fail("An illegal argument was passed to an accessor while finding member");
+      fail("An illegal argument was passed to an accessor while finding member"); //$NON-NLS-1$
     } catch(NoSuchMethodException e) {
       e.printStackTrace();
-      fail("There is no such method while finding member");
+      fail("There is no such method while finding member"); //$NON-NLS-1$
     }
     
     assertNotNull(list);
     
     try {
-      assertEquals("List length should have been 0", 0, list.getValue());
+      assertEquals("List length should have been 0", new Integer(0), list.getValue()); //$NON-NLS-1$
     } catch(IllegalAccessException e) {
       e.printStackTrace();
-      fail("Illegal access while getting value");
+      fail("Illegal access while getting value"); //$NON-NLS-1$
     } catch(InvocationTargetException e) {
       e.printStackTrace();
-      fail("An exception was thrown in a path accessor while getting value");
+      fail("An exception was thrown in a path accessor while getting value"); //$NON-NLS-1$
     } catch(IllegalArgumentException e) {
       e.printStackTrace();
-      fail("An illegal argument was passed to an accessor while getting value");
+      fail("An illegal argument was passed to an accessor while getting value"); //$NON-NLS-1$
     }
   }
   
