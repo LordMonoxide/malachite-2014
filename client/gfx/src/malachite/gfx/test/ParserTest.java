527fd80113bfe491d9ad1feb8608752758e5eaef
Corey Frenette <corey@narwhunderful.com>
2014-08-12 15:17:04 -0300

    New test for late assignments

--- client/gfx/src/malachite/gfx/test/ParserTest.java
+++ client/gfx/src/malachite/gfx/test/ParserTest.java
@@ -12,9 +12,11 @@ import malachite.gfx.gui.parser.Parser;
 
 import org.json.JSONObject;
 import org.junit.Before;
+import org.junit.Ignore;
 import org.junit.Test;
 
 public class ParserTest {
+  @Ignore
   @Test
   public void loadJSONWithSingleControl() {
     String json = 
@@ -48,5 +50,52 @@ public class ParserTest {
     });
     
     _context.run();
+  }
+  
+  @Test
+  public void loadJSONWithLateAssignment() {
+    String json = 
+        "{" +
+          "controls: {" +
+            "login: {" +
+              "type: window," +
+              "text: ~menu.login.title," +
+              "icon: \"#gui.icons.key\"," +
+              "bounds.x: 490," +
+              "bounds.y: 260," +
+              "bounds.w: 300," +
+              "bounds.h: 200," +
+              
+              "controls: {" +
+                "textbox: {" +
+                  "type: textbox," +
+                  "placeholder: Test," +
+                  "bounds.wh.bind_x: @login.content_bounds.wh," +
+                  "bounds.x: 4," +
+                  "bounds.y: 4," +
+                  "bounds.w: -8," +
+                  "bounds.h: 20" +
+                "}" +
+              "}" +
+            "}" +
+          "}" +
+        "}";
+      
+      Manager.registerContext(malachite.gfx.gl21.Context.class);
+      
+      Context _context = Manager.create(ctx -> {
+        ctx.setResizable(true);
+        ctx.setWH(1280, 720);
+        ctx.setFPSTarget(60);
+        ctx.setContextListener(new ContextListenerAdapter() {
+          @Override public void onRun() {
+            Parser parser = new Parser();
+            GUI gui = parser.load(new JSONObject(json));
+            gui.push();
+          }
+        });
+      });
+      
+      _context.run();
   }
 }
