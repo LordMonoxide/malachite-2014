b2bf003bf0be07b7fb92c65a946e7848e3871a86
Corey Frenette <corey@narwhunderful.com>
2014-08-12 21:53:47 -0300

    Fix formatting

--- client/gfx/src/malachite/gfx/test/ParserTest.java
+++ client/gfx/src/malachite/gfx/test/ParserTest.java
@@ -50,47 +50,47 @@ public class ParserTest {
   @Test
   public void loadJSONWithLateAssignment() {
     String json = 
-        "{" +
-          "controls: {" +
-            "login: {" +
-              "type: window," +
-              "text: ~menu.login.title," +
-              "icon: \"#gui.icons.key\"," +
-              "bounds.x: 490," +
-              "bounds.y: 260," +
-              "bounds.w: 300," +
-              "bounds.h: 200," +
-              
-              "controls: {" +
-                "textbox: {" +
-                  "type: textbox," +
-                  "placeholder: Test," +
-                  "bounds.wh.bind_x: @login.content_bounds.wh," +
-                  "bounds.x: 4," +
-                  "bounds.y: 4," +
-                  "bounds.w: -8," +
-                  "bounds.h: 20" +
-                "}" +
+      "{" +
+        "controls: {" +
+          "login: {" +
+            "type: window," +
+            "text: ~menu.login.title," +
+            "icon: \"#gui.icons.key\"," +
+            "bounds.x: 490," +
+            "bounds.y: 260," +
+            "bounds.w: 300," +
+            "bounds.h: 200," +
+            
+            "controls: {" +
+              "textbox: {" +
+                "type: textbox," +
+                "placeholder: Test," +
+                "bounds.wh.bind_x: @login.content_bounds.wh," +
+                "bounds.x: 4," +
+                "bounds.y: 4," +
+                "bounds.w: -8," +
+                "bounds.h: 20" +
               "}" +
             "}" +
           "}" +
-        "}";
-      
-      Manager.registerContext(malachite.gfx.gl21.Context.class);
-      
-      Context _context = Manager.create(ctx -> {
-        ctx.setResizable(true);
-        ctx.setWH(1280, 720);
-        ctx.setFPSTarget(60);
-        ctx.setContextListener(new ContextListenerAdapter() {
-          @Override public void onRun() {
-            Parser parser = new Parser();
-            GUI gui = parser.load(new JSONObject(json));
-            gui.push();
-          }
-        });
+        "}" +
+      "}";
+    
+    Manager.registerContext(malachite.gfx.gl21.Context.class);
+    
+    Context _context = Manager.create(ctx -> {
+      ctx.setResizable(true);
+      ctx.setWH(1280, 720);
+      ctx.setFPSTarget(60);
+      ctx.setContextListener(new ContextListenerAdapter() {
+        @Override public void onRun() {
+          Parser parser = new Parser();
+          GUI gui = parser.load(new JSONObject(json));
+          gui.push();
+        }
       });
-      
-      _context.run();
+    });
+    
+    _context.run();
   }
 }
