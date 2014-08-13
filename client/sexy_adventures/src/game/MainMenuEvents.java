3a7b58232229a4e2c5b4812117da5d0cd79a28ef
Corey Frenette <corey@narwhunderful.com>
2014-08-13 15:16:11 -0300

    Remove old debug stuff

--- client/sexy_adventures/src/game/MainMenuEvents.java
+++ client/sexy_adventures/src/game/MainMenuEvents.java
@@ -18,13 +18,7 @@ public class MainMenuEvents implements GUIEvents {
     btnSubmit   = (Button) controls.get("login_submit");
   }
   
-  public void login(String email, String password, Object test) {
-    System.out.println(email);
-    System.out.println(password);
-    System.out.println(test);
+  public void login(String email, String password) {
     
-    System.out.println(txtEmail.getText());
-    System.out.println(txtPassword.getText());
-    System.out.println(btnSubmit.getText());
   }
 }
