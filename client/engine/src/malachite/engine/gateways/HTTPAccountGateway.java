09b1c78f09e4ebbf7a87adb33c9d33e4963d0c2b
Corey Frenette <corey@narwhunderful.com>
2014-08-14 21:46:06 -0300

    Create http connection in HTTP stuff

--- client/engine/src/malachite/engine/gateways/HTTPAccountGateway.java
+++ client/engine/src/malachite/engine/gateways/HTTPAccountGateway.java
@@ -1,7 +1,25 @@
 package malachite.engine.gateways;
 
+import io.netty.handler.codec.http.HttpResponseStatus;
+import io.netty.handler.codec.http.HttpHeaders;
+
+import com.mastfrog.netty.http.client.HttpClient;
+import com.mastfrog.netty.http.client.ResponseHandler;
+
 public class HTTPAccountGateway implements AccountGatewayInterface {
+  private final HttpClient _http;
+  
+  public HTTPAccountGateway(HttpClient http) {
+    _http = http;
+  }
+  
   @Override public void login(String email, String password) {
-    
+    _http.put()
+     .setURL("http://local.malachite.monoxidedesign.com/api/auth/login")
+     .execute(new ResponseHandler<String>(String.class) {
+       @Override protected void receive(HttpResponseStatus status, HttpHeaders headers, String response) {
+         System.out.println("Here's the response: '" + response + '\'');
+       }
+     });
   }
 }
--- client/engine/src/malachite/engine/providers/HTTPGatewayProvider.java
+++ client/engine/src/malachite/engine/providers/HTTPGatewayProvider.java
@@ -1,13 +1,22 @@
 package malachite.engine.providers;
 
+import com.mastfrog.netty.http.client.HttpClient;
+import com.mastfrog.netty.http.client.HttpClientBuilder;
+
 import malachite.engine.gateways.AccountGatewayInterface;
 import malachite.engine.gateways.HTTPAccountGateway;
 
 public class HTTPGatewayProvider implements GatewayProviderInterface {
   private AccountGatewayInterface _account;
   
+  private HttpClient _http;
+  
+  public HTTPGatewayProvider() {
+    _http = new HttpClientBuilder().build();
+  }
+  
   @Override public AccountGatewayInterface accountGateway() {
-    if(_account == null) { _account = new HTTPAccountGateway(); }
+    if(_account == null) { _account = new HTTPAccountGateway(_http); }
     return _account;
   }
 }
