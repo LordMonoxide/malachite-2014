c6e8a70e5875bda7d5cba858aed2f13aff50d8b1
Corey Frenette <corey@narwhunderful.com>
2014-08-16 01:09:59 -0300

    Server-side login

--- http/app/http/controllers/api/AuthController.php
+++ http/app/http/controllers/api/AuthController.php
@@ -1,9 +1,22 @@
 <?php namespace api;
 
+use Auth;
 use Controller;
+use Input;
+use Response;
+use Validator;
 
 class AuthController extends Controller {
   public function login() {
+    $validator = Validator::make(Input::all(), [
+      'email'    => ['required', 'email', 'exists:users,email'],
+      'password' => ['required', 'min:8', 'max:256']
+    ]);
     
+    if($validator->passes() && Auth::attempt(['email' => Input::get('email'), 'password' => Input::get('password')])) {
+      return Response::json(null, 204);
+    }
+    
+    return Response::json($validator->messages(), 409);
   }
 }
