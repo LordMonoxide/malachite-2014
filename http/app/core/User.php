c9e7adbcb09981085dad4a5f4d0e9fb75de3022b
Corey Frenette <corey@narwhunderful.com>
2014-08-16 08:00:50 -0300

    User migration/model

--- http/app/core/User.php
+++ http/app/core/User.php
@@ -7,21 +7,7 @@ use Illuminate\Auth\Reminders\RemindableTrait;
 use Illuminate\Auth\Reminders\RemindableInterface;
 
 class User extends Eloquent implements UserInterface, RemindableInterface {
-
-	use UserTrait, RemindableTrait;
-
-	/**
-	 * The database table used by the model.
-	 *
-	 * @var string
-	 */
-	protected $table = 'users';
-
-	/**
-	 * The attributes excluded from the model's JSON form.
-	 *
-	 * @var array
-	 */
-	protected $hidden = ['password', 'remember_token'];
-
+  use UserTrait, RemindableTrait;
+  
+  protected $hidden = ['password', 'remember_token'];
 }
