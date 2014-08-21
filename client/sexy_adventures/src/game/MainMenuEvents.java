f8dbb5ec7506d3e263ee5b4f5568572c3221561f
Corey Frenette <corey@narwhunderful.com>
2014-08-21 18:58:56 -0300

    Register menu that doesn't do anything yet

--- client/sexy_adventures/src/game/MainMenuEvents.java
+++ client/sexy_adventures/src/game/MainMenuEvents.java
@@ -6,13 +6,16 @@ import java.util.Objects;
 import malachite.engine.exceptions.AccountException;
 import malachite.engine.gateways.AccountGatewayInterface;
 import malachite.gfx.gui.Control;
+import malachite.gfx.gui.ControlEvents;
 import malachite.gfx.gui.control.Button;
 import malachite.gfx.gui.control.Textbox;
+import malachite.gfx.gui.control.Window;
 import malachite.gfx.gui.parser.GUIEvents;
 
 public class MainMenuEvents implements GUIEvents {
   private AccountGatewayInterface _gateway;
   
+  private Window<?> login, register;
   private Textbox txtEmail;
   private Textbox txtPassword;
   private Button  btnSubmit;
@@ -22,9 +25,16 @@ public class MainMenuEvents implements GUIEvents {
   }
   
   @Override public void registerControls(Map<String, Control<?>> controls) {
+    login    = (Window<?>)controls.get("login");
+    register = (Window<?>)controls.get("register");
+    
     txtEmail    = (Textbox)controls.get("login_email");
     txtPassword = (Textbox)controls.get("login_password");
     btnSubmit   = (Button) controls.get("login_submit");
+  }
+  
+  public void showRegisterClick(ControlEvents.ClickEventData e) {
+    showRegister();
   }
   
   public void login(String email, String password) throws Exception {
@@ -33,5 +43,14 @@ public class MainMenuEvents implements GUIEvents {
     } catch(AccountException.InvalidLoginCredentials e) {
       System.err.println("Invalid email/password");
     }
+  }
+  
+  public void register(String email, String password, String passwordConfirm) throws Exception {
+    
+  }
+  
+  public void showRegister() {
+    login.hide();
+    register.show();
   }
 }
