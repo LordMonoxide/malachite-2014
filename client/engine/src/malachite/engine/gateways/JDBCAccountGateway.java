fcd44674db5ca52ad7e6e55cf4dccee10ab31366
Corey Frenette <corey@narwhunderful.com>
2014-08-22 18:13:05 -0300

    More work on register

--- client/engine/src/malachite/engine/gateways/JDBCAccountGateway.java
+++ client/engine/src/malachite/engine/gateways/JDBCAccountGateway.java
@@ -18,28 +18,30 @@ public class JDBCAccountGateway implements AccountGatewayInterface {
   
   private final Method _prepareStatement;
   
-  private final PreparedStatement _login;
   private final PreparedStatement _exists;
+  private final PreparedStatement _login;
+  private final PreparedStatement _register;
   
   public JDBCAccountGateway(GatewayProviderInterface provider, HasherInterface hasher) {
     _provider = provider;
     _hasher = hasher;
     
     try {
-      _prepareStatement = _provider.getClass().getMethod("prepareStatement", String.class); //$NON-NLS-1$
+      _prepareStatement = _provider.getClass().getMethod("prepareStatement", String.class, boolean.class); //$NON-NLS-1$
     } catch(NoSuchMethodException | SecurityException e) {
       throw new RuntimeException(e);
     }
     
     YeSQL yes = new YeSQL();
     
-    _login  = prepareStatement(yes.table(User.TABLE).select().where(User.DB_EMAIL).equals().limit(1).build());
-    _exists = prepareStatement(yes.table(User.TABLE).count ().where(User.DB_EMAIL).equals().limit(1).build());
+    _exists   = prepareStatement(yes.table(User.TABLE).count ().where(User.DB_EMAIL).equals().limit(1).build(), false);
+    _login    = prepareStatement(yes.table(User.TABLE).select().where(User.DB_EMAIL).equals().limit(1).build(), false);
+    _register = prepareStatement(yes.table(User.TABLE).insert().value(User.DB_EMAIL).value(User.DB_PASSWORD).build(), true);
   }
   
-  private PreparedStatement prepareStatement(String sql) {
+  private PreparedStatement prepareStatement(String sql, boolean retreiveKeys) {
     try {
-      return (PreparedStatement)_prepareStatement.invoke(_provider, sql);
+      return (PreparedStatement)_prepareStatement.invoke(_provider, sql, retreiveKeys);
     } catch(IllegalAccessException | IllegalArgumentException | InvocationTargetException e) {
       throw new RuntimeException(e);
     }
@@ -70,7 +72,9 @@ public class JDBCAccountGateway implements AccountGatewayInterface {
       }
     }
     
-    //TODO: register
+    _register.setString(1, email);
+    _register.setString(2, password);
+    //TODO: Execute, get keys
     
     return null;
   }
