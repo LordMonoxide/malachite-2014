6a876ded67ed103afefc555cdfba4f2827921602
Corey Frenette <corey@narwhunderful.com>
2014-08-22 11:33:41 -0300

    Update yeSQL

--- client/yeSQL/src/yesql/YeSQL.java
+++ client/yeSQL/src/yesql/YeSQL.java
@@ -1,10 +1,8 @@
 package yesql;
 
+import java.util.ArrayList;
+
 public class YeSQL {
-  public static void main(String[] args) {
-    System.out.println(new YeSQL().table("users").select().build());
-  }
-  
   public Table table(String name) {
     return new Table(name);
   }
@@ -26,55 +24,95 @@ public class YeSQL {
     
     public abstract class Query {
       protected final String _type;
+      protected final ArrayList<Where<?>> _where = new ArrayList<>();
       
       private Query(String type) {
         _type = type;
+      }
+      
+      protected String buildWheres() {
+        StringBuilder sql = new StringBuilder();
+        for(int i = 0; i < _where.size(); i++) {
+          if(i == 0) {
+            sql.append(" WHERE ");
+          } else {
+            sql.append(" AND ");
+          }
+          
+          sql.append(_where.get(i).build());
+          
+          if(i + 1 < _where.size()) {
+            sql.append(',');
+          }
+        }
+        
+        return sql.toString();
       }
       
       public abstract String build();
     }
     
     public class Select extends Query {
-      protected final String[] _columns;
+      private final String[] _columns;
+      private int _limit;
       
       private Select(String... columns) {
         super("SELECT");
         _columns = columns;
       }
       
-      public SelectCount count() {
-        return new SelectCount(_columns);
+      public Where<Select> where(String column) {
+        Where<Select> where = new Where<>(this, column);
+        _where.add(where);
+        return where;
       }
       
-      public SelectLimit limit(int limit) {
-        return new SelectLimit(limit, _columns);
-      }
-      
-      @Override public String build() {
-        return _type + ' ' + String.join(",", _columns) + " FROM " + _name;
-      }
-    }
-    
-    public class SelectCount extends Select {
-      private SelectCount(String... columns) {
-        super(columns);
-      }
-      
-      @Override public String build() {
-        return _type + " COUNT(" + String.join(",", _columns) + ") FROM " + _name;
-      }
-    }
-    
-    public class SelectLimit extends Select {
-      private final int _limit;
-      
-      private SelectLimit(int limit, String... columns) {
-        super(columns);
+      public Select limit(int limit) {
         _limit = limit;
+        return this;
       }
       
       @Override public String build() {
-        return _type + ' ' + String.join(",", _columns) + " FROM " + _name + " LIMIT " + _limit;
+        StringBuilder sql = new StringBuilder();
+        sql.append(_type).append(' ').append(String.join(",", _columns)).append(" FROM ").append(_name);
+        
+        if(_where.size() != 0) {
+          sql.append(buildWheres());
+        }
+        
+        if(_limit != 0) {
+          sql.append(" LIMIT ").append(_limit);
+        }
+        
+        return sql.append(';').toString();
+      }
+    }
+    
+    public class Where<T extends Query> {
+      private final T _query;
+      private final String _column;
+      private String _op;
+      private String _value;
+      
+      private Where(T query, String column) {
+        _query  = query;
+        _column = column;
+      }
+      
+      public T equals(String value) {
+        _op    = "=";
+        _value = '\'' + value + '\'';
+        return _query;
+      }
+      
+      public T equals(Number value) {
+        _op    = "=";
+        _value = value.toString();
+        return _query;
+      }
+      
+      private String build() {
+        return _column + _op + _value;
       }
     }
   }
