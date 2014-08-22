0061460ee8fa2f139228fecf1e60f2578654b133
Corey Frenette <corey@narwhunderful.com>
2014-08-22 11:38:19 -0300

    Add table to user model

--- client/engine/src/malachite/engine/models/User.java
+++ client/engine/src/malachite/engine/models/User.java
@@ -1,6 +1,7 @@
 package malachite.engine.models;
 
 public class User {
+  public static final String TABLE       = "users";    //$NON-NLS-1$
   public static final String DB_EMAIL    = "email";    //$NON-NLS-1$
   public static final String DB_PASSWORD = "password"; //$NON-NLS-1$
   
