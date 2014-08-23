66bc4bd130a200a770eee1d68b5847ed7142f525
Corey Frenette <corey@narwhunderful.com>
2014-08-23 10:05:22 -0300

    Register

--- client/engine/src/malachite/engine/models/User.java
+++ client/engine/src/malachite/engine/models/User.java
@@ -2,12 +2,15 @@ package malachite.engine.models;
 
 public class User {
   public static final String TABLE       = "users";    //$NON-NLS-1$
+  public static final String DB_ID       = "id";       //$NON-NLS-1$
   public static final String DB_EMAIL    = "email";    //$NON-NLS-1$
   public static final String DB_PASSWORD = "password"; //$NON-NLS-1$
   
+  public final int    id;
   public final String email;
   
-  public User(String email) {
+  public User(int id, String email) {
+    this.id    = id;
     this.email = email;
   }
 }
