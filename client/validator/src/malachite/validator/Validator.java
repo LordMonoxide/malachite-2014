c090323693fd502613bb5984551cbc21ab55ab5a
Corey Frenette <corey@narwhunderful.com>
2014-08-23 16:29:38 -0300

    Email/password rule

--- client/validator/src/malachite/validator/Validator.java
+++ client/validator/src/malachite/validator/Validator.java
@@ -11,7 +11,24 @@ public class Validator {
   }
   
   static {
-    addRule("email", new Email()); //$NON-NLS-1$
+    addRule("email",    new Email()); //$NON-NLS-1$
+    addRule("password", new Password()); //$NON-NLS-1$
+  }
+  
+  public Validator check(Object value, String... rules) throws ValidatorException {
+    for(String rule : rules) {
+      Rule r = _rules.get(rule);
+      
+      if(r == null) {
+        throw new ValidatorException.NoSuchRule(rule);
+      }
+      
+      if(!r.check(value)) {
+        throw new ValidatorException.ValidationFailure(r, value);
+      }
+    }
+    
+    return this;
   }
   
   public interface Rule {
@@ -21,10 +38,28 @@ public class Validator {
   public static class Email implements Rule {
     @Override public boolean check(Object value) throws ValidatorException {
       if(!(value instanceof String)) {
-        throw new ValidatorException.UnsupportedDatatype("Email", value); //$NON-NLS-1$
+        throw new ValidatorException.UnsupportedDatatype(this, value); //$NON-NLS-1$
       }
       
-      return false;
+      String s = (String)value;
+      if(!s.contains("@")) { return false; }
+      if(s.length() <   3) { return false; }
+      if(s.length() > 254) { return false; }
+      
+      return true;
+    }
+  }
+  
+  public static class Password implements Rule {
+    @Override public boolean check(Object value) throws ValidatorException {
+      if(!(value instanceof String)) {
+        throw new ValidatorException.UnsupportedDatatype(this, value); //$NON-NLS-1$
+      }
+      
+      String s = (String)value;
+      if(s.length() < 6) { return false; }
+      
+      return true;
     }
   }
   
@@ -43,11 +78,27 @@ public class Validator {
       super(message, cause);
     }
     
+    public static class NoSuchRule extends ValidatorException {
+      private static final long serialVersionUID = 1L;
+      
+      public NoSuchRule(String rule) {
+        super("There is no such rule by the name of " + rule); //$NON-NLS-1$
+      }
+    }
+    
+    public static class ValidationFailure extends ValidatorException {
+      private static final long serialVersionUID = 1L;
+      
+      public ValidationFailure(Rule rule, Object value) {
+        super("The validation of " + value + " failed on rule " + rule.getClass().getSimpleName()); //$NON-NLS-1$ //$NON-NLS-2$
+      }
+    }
+    
     public static class UnsupportedDatatype extends ValidatorException {
       private static final long serialVersionUID = 1L;
       
-      public UnsupportedDatatype(String rule, Object value) {
-        super("Unsupported datatype in " + rule + ": " + value.getClass().getName()); //$NON-NLS-1$ //$NON-NLS-2$
+      public UnsupportedDatatype(Rule rule, Object value) {
+        super("Unsupported datatype in " + rule.getClass().getName() + ": " + value.getClass().getSimpleName()); //$NON-NLS-1$ //$NON-NLS-2$
       }
     }
   }
