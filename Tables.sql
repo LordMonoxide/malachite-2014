7f270834237bb53c63024d6ea92ade95bc752893
Corey Frenette <corey@narwhunderful.com>
2014-08-24 10:43:49 -0300

    Update SQL

--- Tables.sql
+++ Tables.sql
@@ -1,5 +1,5 @@
 CREATE TABLE users (
-  id INT NOT NULL AUTO_INCREMENT,
+  id INT UNSIGNED NOT NULL AUTO_INCREMENT,
   email VARCHAR(254) NOT NULL,
   password CHAR(60) NOT NULL,
   
@@ -7,3 +7,12 @@ CREATE TABLE users (
 );
 
 CREATE UNIQUE INDEX unique_users_email ON users (email);
+
+CREATE TABLE characters (
+  user_id INT UNSIGNED NOT NULL,
+  id INT UNSIGNED NOT NULL AUTO_INCREMENT,
+  name VARCHAR(20) NOT NULL,
+  
+  CONSTRAINT pk_characters_id PRIMARY KEY (id),
+  CONSTRAINT fk_user_id       FOREIGN KEY (user_id) REFERENCES users(id)
+);
