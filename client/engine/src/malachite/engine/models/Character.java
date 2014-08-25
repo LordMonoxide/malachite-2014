e05468dc0fe33f93dcd6de54242dd605d1ff0fe0
Corey Frenette <corey@narwhunderful.com>
2014-08-25 14:27:09 -0300

    JDBCCharacter

--- client/engine/src/malachite/engine/models/Character.java
+++ client/engine/src/malachite/engine/models/Character.java
@@ -1,5 +1,15 @@
 package malachite.engine.models;
 
-public class Character {
+import malachite.engine.gateways.AccountGatewayInterface;
+
+public class abstract Character {
+  private final AccountGatewayInterface _gateway;
   
+  public final String name;
+  
+  public Character(AccountGatewayInterface gateway, String name) {
+    _gateway = gateway;
+    
+    this.name = name;
+  }
 }
---
+++ client/engine/src/malachite/engine/models/JDBCCharacter.java
@@ -0,0 +1,26 @@
+package malachite.engine.models;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import malachite.engine.gateways.AccountGatewayInterface;
+
+public class JDBCCharacter extends Character {
+  public static final String TABLE      = "characters"; //$NON-NLS-1$
+  public static final String DB_ID      = "id";         //$NON-NLS-1$
+  public static final String DB_USER_ID = "user_id";    //$NON-NLS-1$
+  public static final String DB_NAME    = "name";       //$NON-NLS-1$
+  
+  public final int  id;
+  public final User user;
+  
+  public JDBCCharacter(AccountGatewayInterface gateway, JDBCUser user, ResultSet r) throws SQLException {
+    this(gateway, r.getInt(DB_ID), user, r.getString(DB_NAME));
+  }
+  
+  public JDBCCharacter(AccountGatewayInterface gateway, int id, JDBCUser user, String name) {
+    super(gateway, name);
+    this.id   = id;
+    this.user = user;
+  }
+}
