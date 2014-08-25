a41154185acfefe7762c9de804272a42d28d0830
Corey Frenette <corey@narwhunderful.com>
2014-08-25 14:26:31 -0300

    Move ID into JDBCUser since User doesn't need to know about it

--- client/engine/src/malachite/engine/models/JDBCUser.java
+++ client/engine/src/malachite/engine/models/JDBCUser.java
@@ -5,12 +5,20 @@ import java.sql.SQLException;
 
 import malachite.engine.gateways.AccountGatewayInterface;
 
-public class JDBCUser extends User<Integer> {
+public class JDBCUser extends User {
+  public static final String TABLE       = "users";    //$NON-NLS-1$
+  public static final String DB_ID       = "id";       //$NON-NLS-1$
+  public static final String DB_EMAIL    = "email";    //$NON-NLS-1$
+  public static final String DB_PASSWORD = "password"; //$NON-NLS-1$
+  
+  public final int id;
+  
   public JDBCUser(AccountGatewayInterface gateway, ResultSet r) throws SQLException {
-    this(gateway, r.getInt(User.DB_ID), r.getString(User.DB_EMAIL));
+    this(gateway, r.getInt(DB_ID), r.getString(DB_EMAIL));
   }
   
   public JDBCUser(AccountGatewayInterface gateway, int id, String email) {
-    super(gateway, Integer.valueOf(id), email);
+    super(gateway, email);
+    this.id = id;
   }
 }
--- client/engine/src/malachite/engine/models/User.java
+++ client/engine/src/malachite/engine/models/User.java
@@ -3,22 +3,15 @@ package malachite.engine.models;
 import malachite.engine.exceptions.AccountException;
 import malachite.engine.gateways.AccountGatewayInterface;
 
-public abstract class User<T> {
-  public static final String TABLE       = "users";    //$NON-NLS-1$
-  public static final String DB_ID       = "id";       //$NON-NLS-1$
-  public static final String DB_EMAIL    = "email";    //$NON-NLS-1$
-  public static final String DB_PASSWORD = "password"; //$NON-NLS-1$
+public abstract class User {
+  private final AccountGatewayInterface _gateway;
   
-  private AccountGatewayInterface _gateway;
-  
-  private T _id;
   private Character[] _chars;
   
   public final String email;
   
-  public User(AccountGatewayInterface gateway, T id, String email) {
+  public User(AccountGatewayInterface gateway, String email) {
     _gateway = gateway;
-    _id      = id;
     
     this.email = email;
   }
