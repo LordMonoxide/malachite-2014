7b9bdcf730db60621d622fe7f4bfe2e51f9c0b03
Corey Frenette <corey@narwhunderful.com>
2014-09-08 21:26:45 -0300

    Nuke old Laravel stuff

--- http/app/gateways/AccountGatewayInterface.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-interface AccountGatewayInterface {
-  public function validateLogin($email, $password);
-  public function login($email, $password);
-}
--- http/app/routes.php
+++
@@ -1,5 +0,0 @@
-<?php
-
-Route::group(['prefix' => 'api', 'namespace' => 'api'], function() {
-  Route::put('/login', ['as' => 'api.login', 'uses' => 'AuthController@login']);
-});
