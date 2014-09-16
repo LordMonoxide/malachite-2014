446b752ff21950c0b91505528d9486825112d180
Corey Frenette <corey@narwhunderful.com>
2014-09-16 14:31:49 -0300

    Default colour (full white, no change) for recolour shader)

--- client/gfx/src/malachite/gfx/shaders/RecolourFragment.java
+++ client/gfx/src/malachite/gfx/shaders/RecolourFragment.java
@@ -2,7 +2,7 @@ package malachite.gfx.shaders;
 
 public class RecolourFragment implements Fragment {
   @Override public String[] getUniforms() {
-    return new String[] {"vec4 recolour"};
+    return new String[] {"vec4 recolour = vec4(1, 1, 1, 1)"};
   }
   
   @Override public String[] outputModifiers() {
